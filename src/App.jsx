baf2be9b6f52633d3a5126962311b659846583eb
Omar Adrian <oadrian38@gmail.com>
2024-02-08 01:18:55 -0600

    Create component nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Nav from './components/Nav';
+
 const App = () => {
     return(
         <>
+            <Nav></Nav>
             <h1>Hola</h1>
             <h2>hello</h2>
         </>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+    return(
+        <div className="Navbar">
+            ola
+        </div>
+    )
+}
+
+export default Nav
